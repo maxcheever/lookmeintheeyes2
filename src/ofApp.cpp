61c5c2207887cf4513e51d2f5ba2bbeb9304d83c
Max Cheever <cheevermax@gmail.com>
2024-01-17 15:44:33 -0500

    comments

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -43,22 +43,14 @@ void ofApp::exit(){
 
 bool compare(ofApp::indexBrightness x, ofApp::indexBrightness y) { return x.brightness < y.brightness; };
 
-// creates array of pixels in face sorted by brightness (ascending) while keeping track of original index
+// creates array of target image pixels sorted by brightness
 void ofApp::makeFaceArr(){
-    
-    /*
-     * this function uses counting sort (https://www.geeksforgeeks.org/counting-sort/)
-     * due to the fact that the difference between the number of values being sorted (786432)
-     * is much larger than the range of values represented (0-255)
-     * essentially, this is nearly O(n) on the array size due to the number of repeated values
-     */
     
     int n = Face.getWidth()*Face.getHeight();
     int m = 0;
     
     vector<indexBrightness> faceBrightness(n);
     
-    // populate temp brightness array
     for (int i = 0; i < n; i++) {
         faceBrightness[i].i = i;
         faceBrightness[i].brightness = Face.getColor(i*3).getBrightness();
@@ -67,17 +59,15 @@ void ofApp::makeFaceArr(){
             m = faceBrightness[i].brightness;
     }
     
+    // counting sort for smoother output see https://github.com/maxcheever/lookmeintheeyes1
     vector<int> count(m+1, 0);
     
-    // count the occurences of each brightness value
     for (int i = 0; i < n; i++)
         count[faceBrightness[i].brightness]++;
     
-    // prefix sum at each index
     for (int i = 1; i <= m; i++)
         count[i] += count[i-1];
     
-    // populate final array
     for (int i = n-1; i >= 0; i--) {
         faceArr[count[faceBrightness[i].brightness]-1] = faceBrightness[i];
         count[faceBrightness[i].brightness]--;
@@ -85,15 +75,9 @@ void ofApp::makeFaceArr(){
     
 }
 
-// makes the output image
+// makes the output frame using the sorted input frame and target image
 void ofApp::makeNewFrame(){
     
-    /*
-     * it would make sense to use counting sort again here due to the fact
-     * since the range of values is still much smaller than the number of values,
-     * but i noticed that using std::sort here creates a distribution of color
-     * that is much smoother in the output (std::sort is still efficient at O(nlogn))
-     */
     
     int iWidth = Face.getWidth();
     int iHeight = Face.getHeight();
@@ -103,17 +87,17 @@ void ofApp::makeNewFrame(){
     
     vector<indexBrightness> sampleBrightness(n);
     
-    // populate temp brightness array
     for (int i = 0; i < n; i++) {
         sampleBrightness[i].i = i;
         sampleBrightness[i].brightness = src.getColor(i*3).getBrightness();
     }
     
+    // intro sort for smoother output see https://github.com/maxcheever/lookmeintheeyes1
     sort(sampleBrightness.begin(), sampleBrightness.end(), compare);
     
     dest.allocate(iWidth, iHeight, OF_IMAGE_COLOR);
     
-    // map pixels to the correct position
+    // map pixels to the correct position in output frame
     for (int i = 0; i < n; i++) {
         dest.setColor(faceArr[i].i*3, src.getColor(sampleBrightness[i].i*3));
     }
