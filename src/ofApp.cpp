281b6f2ed4d508d430ece9f8c0d5109f995b82e7
Max Cheever <cheevermax@gmail.com>
2024-01-16 22:40:35 -0500

    well that was easier than i thought lol

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -3,7 +3,12 @@
 //--------------------------------------------------------------
 void ofApp::setup(){
     
-    Face.load("face3.jpg");
+    camWidth = 1024;
+    camHeight = 768;
+    videoGrabber.initGrabber(camWidth,camHeight);
+    texture.allocate(camWidth,camHeight, GL_RGB);
+    
+    Face.load("face4.jpg");
     // Face.setImageType(OF_IMAGE_GRAYSCALE);
     makeFaceArr();
 
@@ -11,11 +16,23 @@ void ofApp::setup(){
 
 //--------------------------------------------------------------
 void ofApp::update(){
+    
+    videoGrabber.update();
+    
+    if(videoGrabber.isFrameNew()){
+        makeNewFrame();
+    }
 
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
+    
+    ofBackground(100,100,100);
+    ofSetColor(255,255,255);
+
+    texture.draw(0, 0);
+    videoGrabber.draw(0,0, 256, 192);
 
 }
 
@@ -23,6 +40,8 @@ void ofApp::draw(){
 void ofApp::exit(){
 
 }
+
+bool compare(ofApp::indexBrightness x, ofApp::indexBrightness y) { return x.brightness < y.brightness; };
 
 // creates array of pixels in face sorted by brightness (ascending) while keeping track of original index
 void ofApp::makeFaceArr(){
@@ -63,5 +82,42 @@ void ofApp::makeFaceArr(){
         faceArr[count[faceBrightness[i].brightness]-1] = faceBrightness[i];
         count[faceBrightness[i].brightness]--;
     }
+    
+}
+
+// makes the output image
+void ofApp::makeNewFrame(){
+    
+    /*
+     * it would make sense to use counting sort again here due to the fact
+     * since the range of values is still much smaller than the number of values,
+     * but i noticed that using std::sort here creates a distribution of color
+     * that is much smoother in the output (std::sort is still efficient at O(nlogn))
+     */
+    
+    int iWidth = Face.getWidth();
+    int iHeight = Face.getHeight();
+    int n = iWidth*iHeight;
+    ofPixels src = videoGrabber.getPixels();
+    
+    
+    vector<indexBrightness> sampleBrightness(n);
+    
+    // populate temp brightness array
+    for (int i = 0; i < n; i++) {
+        sampleBrightness[i].i = i;
+        sampleBrightness[i].brightness = src.getColor(i*3).getBrightness();
+    }
+    
+    sort(sampleBrightness.begin(), sampleBrightness.end(), compare);
+    
+    dest.allocate(iWidth, iHeight, OF_IMAGE_COLOR);
+    
+    // map pixels to the correct position
+    for (int i = 0; i < n; i++) {
+        dest.setColor(faceArr[i].i*3, src.getColor(sampleBrightness[i].i*3));
+    }
+    
+    texture.loadData(dest);
     
 }
